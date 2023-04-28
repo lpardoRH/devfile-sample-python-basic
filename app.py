a9095abc1c8fe498866aa4c5b0f050339be7933c
Laura Pardo <37450412+lpardoRH@users.noreply.github.com>
2023-04-28 20:35:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, render_template_string
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def hello():
     if request.args.get('c'):
         return render_template_string(request.args.get('c'))
     else:
         return "Hello!"
 
 if __name__ == "__main__":
-    app.run()
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+
+    app.run(port=port,host='0.0.0.0')
